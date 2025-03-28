365d200fc01001eaede9035d26a80b241a49a008
Evgenii Kolosov <kolosov.e.s@gmail.com>
2025-03-28 18:46:39 +0100

    VCST-2980: Copy UI blade functionality

--- src/VirtoCommerce.OrderManagement.Web/Scripts/module.js
+++ src/VirtoCommerce.OrderManagement.Web/Scripts/module.js
@@ -6,38 +6,81 @@ if (AppDependencies !== undefined) {
 }
 
 angular.module(moduleName, [])
-    .config(['$stateProvider',
-        function ($stateProvider) {
-            $stateProvider
-                .state('workspace.OrderManagementState', {
-                    url: '/OrderManagement',
-                    templateUrl: '$(Platform)/Scripts/common/templates/home.tpl.html',
-                    controller: [
-                        'platformWebApp.bladeNavigationService',
-                        function (bladeNavigationService) {
-                            var newBlade = {
-                                id: 'blade1',
-                                controller: 'OrderManagement.helloWorldController',
-                                template: 'Modules/$(VirtoCommerce.OrderManagement)/Scripts/blades/hello-world.html',
-                                isClosingDisabled: true,
-                            };
-                            bladeNavigationService.showBlade(newBlade);
-                        }
-                    ]
-                });
-        }
-    ])
-    .run(['platformWebApp.mainMenuService', '$state',
-        function (mainMenuService, $state) {
+//    .config(['$stateProvider',
+//        function ($stateProvider) {
+//            $stateProvider
+//                .state('workspace.OrderManagementState', {
+//                    url: '/OrderManagement',
+//                    templateUrl: '$(Platform)/Scripts/common/templates/home.tpl.html',
+//                    controller: [
+//                        'platformWebApp.bladeNavigationService',
+//                        function (bladeNavigationService) {
+//                            var newBlade = {
+//                                id: 'blade1',
+//                                controller: 'OrderManagement.helloWorldController',
+//                                template: 'Modules/$(VirtoCommerce.OrderManagement)/Scripts/blades/hello-world.html',
+//                                isClosingDisabled: true,
+//                            };
+//                            bladeNavigationService.showBlade(newBlade);
+//                        }
+//                    ]
+//                });
+//        }
+//    ])
+    .run(['platformWebApp.mainMenuService', '$state', 'platformWebApp.toolbarService', 'platformWebApp.bladeNavigationService',
+        function (mainMenuService, $state, toolbarService, bladeNavigationService) {
             //Register module in main menu
-            var menuItem = {
-                path: 'browse/OrderManagement',
-                icon: 'fa fa-cube',
-                title: 'OrderManagement',
-                priority: 100,
-                action: function () { $state.go('workspace.OrderManagementState'); },
-                permission: 'OrderManagement:access',
-            };
-            mainMenuService.addMenuItem(menuItem);
+//            var menuItem = {
+//                path: 'browse/OrderManagement',
+//                icon: 'fa fa-cube',
+//                title: 'OrderManagement',
+//                priority: 100,
+//                action: function () { $state.go('workspace.OrderManagementState'); },
+//                permission: 'OrderManagement:access',
+//            };
+//            mainMenuService.addMenuItem(menuItem);
+
+            toolbarService.register({
+                name: "OrderManagement.commands.add-item",
+                icon: 'fas fa-plus',
+                executeMethod: function () {
+                    var bladeAddItem = {
+                        id: 'bladeAddItem',
+                        controller: 'OrderManagement.helloWorldController',
+                        template: 'Modules/$(VirtoCommerce.OrderManagement)/Scripts/blades/hello-world.html'
+                    };
+                    bladeNavigationService.showBlade(bladeAddItem);
+                },
+                canExecuteMethod: function (blade) {
+                    return blade.currentEntity.operationType === 'CustomerOrder';
+                },
+                permission: 'order:update',
+                index: 0
+            }, 'virtoCommerce.orderModule.customerOrderItemsController');
+
+            toolbarService.register({
+                name: "platform.commands.remove",
+                icon: 'fas fa-trash-alt',
+                executeMethod: function (blade) {
+                    var lineItems = blade.currentEntity.items;
+                    var selectedLineItems = _.filter(lineItems, function (x) { return x.selected; });
+
+                    if (blade.selectedNodeId >= 0) {
+                        var selectedNode = lineItems[blade.selectedNodeId];
+                        if (selectedNode && _.some(selectedLineItems, lineItem => selectedNode.id === lineItem.id)) {
+                            bladeNavigationService.closeChildrenBlades(blade);
+                        }
+                    }
+
+                    blade.currentEntity.items = _.difference(lineItems, selectedLineItems);
+                    blade.selectedAll = false;
+                    blade.recalculateFn();
+                },
+                canExecuteMethod: function (blade) {
+                    return _.any(blade.currentEntity.items, function (x) { return x.selected; });
+                },
+                permission: 'order:update',
+                index: 1
+            }, 'virtoCommerce.orderModule.customerOrderItemsController');
         }
     ]);
