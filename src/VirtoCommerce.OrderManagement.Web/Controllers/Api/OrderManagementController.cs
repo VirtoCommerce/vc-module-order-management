eefbd8bc3fa3b7e467417ff3916185b43a101434
Evgenii Kolosov <108291208+Ljutyj@users.noreply.github.com>
2025-05-12 19:44:07 +0300

    VCST-2638: Getting the price from the x-catalog and a single point of adding the product to the order (#2)
    Co-authored-by: Andrew <56427313+AndrewEhlo@users.noreply.github.com>

--- src/VirtoCommerce.OrderManagement.Web/Controllers/Api/OrderManagementController.cs
+++ src/VirtoCommerce.OrderManagement.Web/Controllers/Api/OrderManagementController.cs
@@ -1,9 +1,97 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using VirtoCommerce.CatalogModule.Core.Services;
+using VirtoCommerce.OrdersModule.Core.Model;
+using VirtoCommerce.OrdersModule.Core.Services;
+using VirtoCommerce.OrdersModule.Data.Authorization;
+using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.XCatalog.Core.Queries;
+using Permissions = VirtoCommerce.OrderManagement.Core.ModuleConstants.Security.Permissions;
 
 namespace VirtoCommerce.OrderManagement.Web.Controllers.Api
 {
     [Route("api/order-management")]
-    public class OrderManagementController : Controller
+    public class OrderManagementController(
+        IMediator mediator,
+        IItemService itemsService,
+        IAuthorizationService authorizationService,
+        ICustomerOrderService customerOrderService) : Controller
     {
+        private readonly IMediator _mediator = mediator;
+        private readonly IItemService _itemsService = itemsService;
+        private readonly IAuthorizationService _authorizationService = authorizationService;
+        private readonly ICustomerOrderService _customerOrderService = customerOrderService;
+
+        [HttpPut]
+        [Route("add-items/{orderId}")]
+        public async Task<ActionResult<CustomerOrder>> AddOrderItems([FromRoute] string orderId, [FromBody] List<string> productIds, CancellationToken cancellationToken)
+        {
+            if (productIds.IsNullOrEmpty())
+            {
+                return BadRequest("ProductIds list cannot be null or empty.");
+            }
+
+            var order = await _customerOrderService.GetByIdAsync(orderId, CustomerOrderResponseGroup.Full.ToString());
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            var authorizationResult = await _authorizationService.AuthorizeAsync(User, order, new OrderAuthorizationRequirement(Permissions.Update));
+            if (!authorizationResult.Succeeded)
+            {
+                return Forbid();
+            }
+
+            var products = await _itemsService.GetNoCloneAsync(productIds);
+            if (products.IsNullOrEmpty())
+            {
+                return NotFound();
+            }
+
+            var searchProductQuery = AbstractTypeFactory<SearchProductQuery>.TryCreateInstance();
+            searchProductQuery.StoreId = order.StoreId;
+            searchProductQuery.CultureName = order.LanguageCode;
+            searchProductQuery.CurrencyCode = order.Currency;
+            searchProductQuery.Filter = "availability:InStock";
+            searchProductQuery.ObjectIds = products.Select(x => x.Id).ToArray();
+            searchProductQuery.IncludeFields = ["items.id", "items.price.discountAmount.amount", "items.price.list.amount", "items.price.currency"];
+
+            var searchProductResponse = await _mediator.Send(searchProductQuery, cancellationToken);
+
+            foreach (var product in products)
+            {
+                var lineItem = AbstractTypeFactory<LineItem>.TryCreateInstance();
+                lineItem.ProductId = product.Id;
+                lineItem.CatalogId = product.CatalogId;
+                lineItem.CategoryId = product.CategoryId;
+                lineItem.Name = product.Name;
+                lineItem.ImageUrl = product.ImgSrc;
+                lineItem.Sku = product.Code;
+                lineItem.Quantity = 1;
+                lineItem.Currency = order.Currency;
+                lineItem.Price = 0;
+                lineItem.DiscountAmount = 0;
+
+                var productPrice = searchProductResponse.Results.FirstOrDefault(x => x.Id == product.Id);
+                if (productPrice != null)
+                {
+                    var price = productPrice.AllPrices.FirstOrDefault(x => x.Currency.Code == order.Currency);
+                    lineItem.Price = price?.ListPrice?.Amount ?? 0;
+                    lineItem.DiscountAmount = price?.DiscountAmount?.Amount ?? 0;
+                }
+
+                order.Items.Add(lineItem);
+            }
+
+            await _customerOrderService.SaveChangesAsync([order]);
+
+            return Ok(order);
+        }
     }
 }
